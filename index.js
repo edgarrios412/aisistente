8a700e38410535f64c2ad99bc534131ff675126c
unknown <edgarrios412@gmail.com>
2024-05-29 11:26:11 -0500

    change asistente

--- index.js
+++ index.js
@@ -7,28 +7,27 @@ const cors = require("cors")
 const app = express()
 
 app.use(cors())
-
+app.use(express.json())
 // const options2 = {
 //     text: "With the imageGeneration function, you will be able to generate images from a text input and optional parameters that will provide you with millions of combinations to stylize each of the images.",
 //     source: "en",
 //     target: "es"
 // };
 
-const fn = async (msg) => {
-    const messages = [
-        { role: "assistant", content: "Te llamas Edgar Vilchez, tienes 22 años, vives en Colombia Bogotá, localidad Rafael Uribe Uribe, eres desarrollador Fullstack con las tecnologias ReactJS, nodeJS, postgresql, react native, en caso de ser necesario puedes mudarte a otro lugar, aprendes muy rápido, respondes amablemente y respondes brevemente pero muy amable, menos de 100 caracteres, tu objetivo es conseguir trabajo pero sin demostrar la necesidad, tienes 2 años de experiencia en la programación aunque te gusta programar desde los 15 años, pueden contactarte por correo edgarrios412@gmail.com o por linkedIn el url es: https://www.linkedin.com/in/edgarrios412/ si preguntan algo y no tienes la respuesta di que me contacten"},
-        { role: "user", content: msg}
-    ];
-    const response = await g4f.chatCompletion(messages)
+const fn = async (conversation) => {
+    // const messages = [
+    //     { role: "assistant", content: "Te llamas Edgar Vilchez, tienes 22 años, vives en Colombia Bogotá, localidad Rafael Uribe Uribe, eres desarrollador Fullstack con las tecnologias ReactJS, nodeJS, postgresql, react native, en caso de ser necesario puedes mudarte a otro lugar, aprendes muy rápido, respondes amablemente y respondes brevemente pero muy amable, menos de 100 caracteres, tu objetivo es conseguir trabajo pero sin demostrar la necesidad, tienes 2 años de experiencia en la programación aunque te gusta programar desde los 15 años, pueden contactarte por correo edgarrios412@gmail.com o por linkedIn el url es: https://www.linkedin.com/in/edgarrios412/ si preguntan algo y no tienes la respuesta di que me contacten"},
+    //     { role: "user", content: msg}
+    // ];
+    const response = await g4f.chatCompletion(conversation)
     return response
 }
 
 // g4f.translation(options2).then(console.log)
 
-app.get("/edgarvilchez", async (req,res) => {
-    const {msg} = req.query
-    console.log(req.query)
-    const response = await fn(msg)
+app.post("/edgarvilchez", async (req,res) => {
+    const {conversation} = req.body
+    const response = await fn(conversation)
     res.json(response)
 })
 
